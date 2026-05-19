fd651e98c5b5b76ee3229830e85c0a7effbc1ffb
José Kennedy <josek3011@gmail.com>
2026-05-19 18:12:44 -0300

    corrige nome do middleware whitenoise

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m#q6vz1*hh_!*xfr)ubsq-*7mh1067ec0(6b7jr!ea-=o$7oia'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
